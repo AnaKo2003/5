a3b74907941e7aff6e99c7ccd10d6d7f78f1da52
Анко <pannikovaanna2000@mail.ru>
2022-10-05 21:43:16 +0300

    Initial commit

---
+++ 4.py
@@ -0,0 +1,97 @@
+# Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
+# Входные и выходные данные хранятся в отдельных текстовых файлах.
+
+def rle_for_numbers(data): 
+    string = ''
+    cout = 1
+    for i in range(len(string)-1):
+        if i <= len(string):
+            if string[i] == string[i + 1]:
+                cout += 1
+            else:
+                a = string[i]
+                print(cout, string[i])
+                cout = 1
+    print(cout, string[i])
+
+def rle_encode(data): 
+    encoding = ''
+    prev_char = ''
+    count = 1
+    if not data: return ''
+    for char in data:
+        if char != prev_char:
+            if prev_char:
+                encoding += str(count) + prev_char
+            count = 1
+            prev_char = char
+        else:
+            count += 1
+    else:
+        encoding += str(count) + prev_char
+        return encoding
+
+def rle_decode(data):
+    decode = ''
+    count = ''
+    for char in data:
+        if char.isdigit():
+            count += char
+        else:
+            decode += char * int(count)
+            count = ''
+    return decode
+
+with open("RLEencode.txt", "r") as enterFile:
+    enterData = enterFile.readline()
+print(f'Ваша запись во входящем файле: {enterData}')
+
+encoded_val = rle_encode(enterData) 
+print(f'Ваша запись после RLE-сжатия: {encoded_val}')
+
+decoded_val = rle_decode(encoded_val)
+print(f'Ваша запись после RLE-восстановления: {decoded_val}')
+
+with open("RLEdecode.txt", "w") as exitFile:
+    exitFile.write(decoded_val)
+
+enterFile.close()
+exitFile.close()
+
+
+# def Compression(text): 
+#     compresdata = ''
+
+#     i = 0
+#     while i < len(text):
+#         count = 1
+#         while i + 1 < len(text) and text[i] == text[i + 1]:
+#             count += 1
+#             i += 1
+#         compresdata += str(count) + text[i]
+#         i += 1
+    
+#     return compresdata
+
+
+# def Recovery(text): 
+#     datarecovery = ''
+
+#     i = 0
+#     while i < len(text):
+#         datarecovery += str(text[i+1]) * int(text[i])
+#         i+=2
+    
+#     return datarecovery
+
+
+# with open('Text1.txt', 'r') as t1:
+#     t1 = t1.read()    
+
+# with open('Text2.txt', 'w+') as t2:
+#     t2.write(Compression(t1))
+#     t2.seek(0)                     
+#     t2 = t2.read()
+ 
+# with open('Text3.txt', 'w') as t3:
+#     t3.write(Recovery(t2))
